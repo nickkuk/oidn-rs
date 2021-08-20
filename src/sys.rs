b0c7488945e17dfc2e2955030f75d18e9735937e
nickkuk <nickkuklin@gmail.com>
2021-08-20 13:55:32 +0500

    Test stdcall convention.

--- src/sys.rs
+++ src/sys.rs
@@ -13,7 +13,7 @@ pub const OIDNError_OIDN_ERROR_UNSUPPORTED_HARDWARE: OIDNError = 5;
 pub const OIDNError_OIDN_ERROR_CANCELLED: OIDNError = 6;
 pub type OIDNError = ::std::os::raw::c_uint;
 pub type OIDNErrorFunction = ::std::option::Option<
-    unsafe extern "system" fn(
+    unsafe extern "stdcall" fn(
         userPtr: *mut ::std::os::raw::c_void,
         code: OIDNError,
         message: *const ::std::os::raw::c_char,
@@ -25,48 +25,48 @@ pub struct OIDNDeviceImpl {
     _unused: [u8; 0],
 }
 pub type OIDNDevice = *mut OIDNDeviceImpl;
-extern "system" {
+extern "stdcall" {
     pub fn oidnNewDevice(type_: OIDNDeviceType) -> OIDNDevice;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnRetainDevice(device: OIDNDevice);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnReleaseDevice(device: OIDNDevice);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetDevice1b(device: OIDNDevice, name: *const ::std::os::raw::c_char, value: bool);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetDevice1i(
         device: OIDNDevice,
         name: *const ::std::os::raw::c_char,
         value: ::std::os::raw::c_int,
     );
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnGetDevice1b(device: OIDNDevice, name: *const ::std::os::raw::c_char) -> bool;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnGetDevice1i(
         device: OIDNDevice,
         name: *const ::std::os::raw::c_char,
     ) -> ::std::os::raw::c_int;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetDeviceErrorFunction(
         device: OIDNDevice,
         func: OIDNErrorFunction,
         userPtr: *mut ::std::os::raw::c_void,
     );
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnGetDeviceError(
         device: OIDNDevice,
         outMessage: *mut *const ::std::os::raw::c_char,
     ) -> OIDNError;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnCommitDevice(device: OIDNDevice);
 }
 pub const OIDNFormat_OIDN_FORMAT_UNDEFINED: OIDNFormat = 0;
@@ -86,17 +86,17 @@ pub struct OIDNBufferImpl {
     _unused: [u8; 0],
 }
 pub type OIDNBuffer = *mut OIDNBufferImpl;
-extern "system" {
+extern "stdcall" {
     pub fn oidnNewBuffer(device: OIDNDevice, byteSize: size_t) -> OIDNBuffer;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnNewSharedBuffer(
         device: OIDNDevice,
         ptr: *mut ::std::os::raw::c_void,
         byteSize: size_t,
     ) -> OIDNBuffer;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnMapBuffer(
         buffer: OIDNBuffer,
         access: OIDNAccess,
@@ -104,17 +104,17 @@ extern "system" {
         byteSize: size_t,
     ) -> *mut ::std::os::raw::c_void;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnUnmapBuffer(buffer: OIDNBuffer, mappedPtr: *mut ::std::os::raw::c_void);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnRetainBuffer(buffer: OIDNBuffer);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnReleaseBuffer(buffer: OIDNBuffer);
 }
 pub type OIDNProgressMonitorFunction = ::std::option::Option<
-    unsafe extern "system" fn(userPtr: *mut ::std::os::raw::c_void, n: f64) -> bool,
+    unsafe extern "stdcall" fn(userPtr: *mut ::std::os::raw::c_void, n: f64) -> bool,
 >;
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
@@ -122,16 +122,16 @@ pub struct OIDNFilterImpl {
     _unused: [u8; 0],
 }
 pub type OIDNFilter = *mut OIDNFilterImpl;
-extern "system" {
+extern "stdcall" {
     pub fn oidnNewFilter(device: OIDNDevice, type_: *const ::std::os::raw::c_char) -> OIDNFilter;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnRetainFilter(filter: OIDNFilter);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnReleaseFilter(filter: OIDNFilter);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetFilterImage(
         filter: OIDNFilter,
         name: *const ::std::os::raw::c_char,
@@ -144,7 +144,7 @@ extern "system" {
         byteRowStride: size_t,
     );
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetSharedFilterImage(
         filter: OIDNFilter,
         name: *const ::std::os::raw::c_char,
@@ -157,10 +157,10 @@ extern "system" {
         byteRowStride: size_t,
     );
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnRemoveFilterImage(filter: OIDNFilter, name: *const ::std::os::raw::c_char);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetSharedFilterData(
         filter: OIDNFilter,
         name: *const ::std::os::raw::c_char,
@@ -168,47 +168,47 @@ extern "system" {
         byteSize: size_t,
     );
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnUpdateFilterData(filter: OIDNFilter, name: *const ::std::os::raw::c_char);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnRemoveFilterData(filter: OIDNFilter, name: *const ::std::os::raw::c_char);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetFilter1b(filter: OIDNFilter, name: *const ::std::os::raw::c_char, value: bool);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnGetFilter1b(filter: OIDNFilter, name: *const ::std::os::raw::c_char) -> bool;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetFilter1i(
         filter: OIDNFilter,
         name: *const ::std::os::raw::c_char,
         value: ::std::os::raw::c_int,
     );
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnGetFilter1i(
         filter: OIDNFilter,
         name: *const ::std::os::raw::c_char,
     ) -> ::std::os::raw::c_int;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetFilter1f(filter: OIDNFilter, name: *const ::std::os::raw::c_char, value: f32);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnGetFilter1f(filter: OIDNFilter, name: *const ::std::os::raw::c_char) -> f32;
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnSetFilterProgressMonitorFunction(
         filter: OIDNFilter,
         func: OIDNProgressMonitorFunction,
         userPtr: *mut ::std::os::raw::c_void,
     );
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnCommitFilter(filter: OIDNFilter);
 }
-extern "system" {
+extern "stdcall" {
     pub fn oidnExecuteFilter(filter: OIDNFilter);
 }
